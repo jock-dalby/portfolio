33e83507aafcaca077f7deb5747a8198175acf00
jock-dalby <jockdalby86@gmail.com>
2016-12-22 15:46:02 +1300

    complete mobile first layout

---
+++ script.js
@@ -0,0 +1,14 @@
+
+  // const jobTitle = document.querySelector('.jobTitle');
+  // const jobTitleTimer = setInterval(rotateJobTitle, 5000);
+  // const jobTitleOptions = ["Web Developer", "Entrepreneur", "Business Owner", "Manager"]
+  // let currentTitle = 1;
+  //
+  // function rotateJobTitle () {
+  //     jobTitle.innerText=jobTitleOptions[currentTitle];
+  //       if(currentTitle === jobTitleOptions.length - 1) {
+  //       currentTitle = 0;
+  //     } else {
+  //       currentTitle++;
+  //     }
+  // }
