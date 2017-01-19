f2f9f11ba379086d7764672a86e1a5b213ded4a9
jock-dalby <jockdalby86@gmail.com>
2017-01-20 10:38:03 +1300

    update cv

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-$(document).click(function (event) {
-    var clickover = $(event.target);
-    var $navbar = $(".navbar-collapse");
-    var _opened = $navbar.hasClass("in");
-    if (_opened === true && !clickover.hasClass("navbar-toggle")) {
-        $navbar.collapse('hide');
+
+
+// biopic moving shadow
+const biopic = document.querySelector('.profile-pic-container');
+const img = biopic.querySelector('#profile-pic');
+const shadowRange = 30;
+
+function shadowChange(e) {
+    const { offsetWidth: width, offsetHeight: height } = biopic;
+    let { offsetX: x, offsetY: y} = e;
+
+    if(this !== e.target) {
+      x = x + e.target.offsetLeft;
+      y = y + e.target.offsetTop;
     }
 
-});
+    const xShadow = Math.round((x / width * shadowRange) - (shadowRange / 2));
+    const yShadow = Math.round((y / height * shadowRange) - (shadowRange / 2));
+
+    img.style.boxShadow = `
+      ${xShadow}px ${yShadow}px 0 rgba(236, 79, 60, 0.7)
+    `
+}
+
+biopic.addEventListener('mousemove', shadowChange);
+
+
+// Mobile navbar
+const navbar = document.querySelector('.navbar-collapse')
+
+navbar.addEventListener('click', (e) => {
+    if (e.target.classList.contains('.navbar-toggle') || e.target.getElementsByTagName("a")) {
+        navbar.classList.toggle('in');
+    }
+
+})
